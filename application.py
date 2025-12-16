efc6a4607908dfc26bf2a371f4b06d6dd633fd04
sahilmehta50 <64104969+sahilmehta50@users.noreply.github.com>
2025-12-16 21:50:46 +0530

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "EB + GitHub Actions = SUCCESS ✅"
+    return "<h1>EB + GitHub Actions SUCCESS ✅</h1>"
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=8080)
